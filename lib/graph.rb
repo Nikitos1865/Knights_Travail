74ef24e8d71157dbcfaf2c4a2d6f4443aa2de7cd
Nikitos1865 <na2789@columbia.edu>
2024-04-02 21:04:16 -0400

    revamped the bfs to actually work as intended, to print out the distance from the origin, and the predecessor nodes for each node from the target. The way it works is kind of like Dijkstra's Algorithm except without weights.

--- lib/graph.rb
+++ lib/graph.rb
@@ -17,6 +17,69 @@ class Graph
         vertices.each {|v| v.append}
     end 
 
+    def dfs(v, visited = [])
+        vert = get_v(v)
+        visited << v 
+        puts v.to_s + " "
+
+        vert.neighbors.each do |neighbor|
+            if !visited.include?(neighbor)
+                dfs(neighbor, visited)
+            end 
+        end 
+
+
+    end 
+
+    def bfs(origin)
+
+        bfs_info = {}
+
+        #below method initiates the bfs_info hash which takes the coordinate as the key, and 
+        #sets the distance and predecessor to nil
+        vertices.each do |info|
+            bfs_info[info.coord] = [distance = nil, predecessor = nil]
+        end 
+
+        bfs_info[origin] = [0,nil]
+
+        queue = []
+
+        queue << origin
+
+        while !queue.empty? 
+            u = queue.shift
+
+            
+            get_v(u).neighbors.each do |neighbor|
+                if bfs_info[neighbor][0] == nil
+                    bfs_info[neighbor] = [bfs_info[u][0]+1, u]
+                    queue << neighbor
+                end 
+            end 
+        end 
+
+        bfs_info
+        
+    end 
+
+    def knights_move(origin, dest, arr = [dest])
+        bfs = bfs(origin)
+
+        arr << precursor = bfs[dest][1]
+
+        knights_move(origin, precursor, arr) unless precursor == origin
+
+        arr
+    end 
+
+    
+
+    def get_v(coord)
+        v = vertices.select {|v| v.coord == coord}
+        v[0]
+    end 
+
 
 end 
 
@@ -24,6 +87,20 @@ graph = Graph.new
 
 
 
-graph.vertices.each {|v| p "#{v.coord}:    #{v.vertex[v.coord]}"}
+
+graph.bfs([3,3]).each do |v|
+    p v
+end 
+
+p graph.knights_move([3,3],[4,3])
+
+
+#graph.vertices.each {|v| p "#{v.coord}:  #{v.neighbors}"}
+
+
+
+
+
+
     
 
--- lib/vertex.rb
+++ lib/vertex.rb
@@ -1,25 +1,22 @@
 class Vertex
-    attr_accessor :vertex, :coord
+    attr_accessor :coord, :neighbors
 
     def initialize(coord = [])
         @coord = [coord[0], coord[1]]
-        @vertex = {coord => []}
+        @neighbors = []
     end 
 
     def append
-        self.vertex[coord] <<  [@coord[0]-1, @coord[1]-2] unless (coord[0] < 1 || @coord[1] < 2)
-        self.vertex[coord] << [@coord[0]+1, @coord[1]-2] unless (coord[0] > 6 || @coord[1] < 2)
-        self.vertex[coord] <<  [@coord[0]-1, @coord[1]+2] unless (coord[0] < 1 || @coord[1] > 5)
-        self.vertex[coord] << [@coord[0]+1, @coord[1]+2] unless (coord[0] > 6 || @coord[1] > 5)
-        self.vertex[coord] << [@coord[0]-2, @coord[1]-1] unless (coord[0] < 2 || @coord[1] < 1)
-        self.vertex[coord] <<  [@coord[0]-2, @coord[1]+1] unless (coord[0] < 2 || @coord[1] > 6)
-        self.vertex[coord] << [@coord[0]+2, @coord[1]-1] unless (coord[0] > 5 || @coord[1] < 1)
-        self.vertex[coord] << [@coord[0]+2, @coord[1]+1] unless (coord[0] > 5 || @coord[1] > 6)
+        @neighbors <<  [@coord[0]-1, @coord[1]-2] unless (coord[0] < 1 || @coord[1] < 2)
+        @neighbors << [@coord[0]+1, @coord[1]-2] unless (coord[0] > 6 || @coord[1] < 2)
+        @neighbors <<  [@coord[0]-1, @coord[1]+2] unless (coord[0] < 1 || @coord[1] > 5)
+        @neighbors << [@coord[0]+1, @coord[1]+2] unless (coord[0] > 6 || @coord[1] > 5)
+        @neighbors << [@coord[0]-2, @coord[1]-1] unless (coord[0] < 2 || @coord[1] < 1)
+        @neighbors <<  [@coord[0]-2, @coord[1]+1] unless (coord[0] < 2 || @coord[1] > 6)
+        @neighbors << [@coord[0]+2, @coord[1]-1] unless (coord[0] > 5 || @coord[1] < 1)
+        @neighbors << [@coord[0]+2, @coord[1]+1] unless (coord[0] > 5 || @coord[1] > 6)
     end 
 
 end 
 
-v = Vertex.new([3,3])
-
-v.append
 
